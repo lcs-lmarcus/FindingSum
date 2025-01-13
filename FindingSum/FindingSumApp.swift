ab431e5abb31840c56337236a27d72714fd03e76
Marcus Li <mmli@lcs.on.ca>
2025-01-13 09:14:18 -0500

    Initial Commit

---
+++ FindingSum/FindingSumApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindingSumApp.swift
+//  FindingSum
+//
+//  Created by Marcus Li on 2025-01-13.
+//
+
+import SwiftUI
+
+@main
+struct FindingSumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
